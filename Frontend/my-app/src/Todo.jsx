4cb3db1e9a62c1f3f803510758e673011d11afaf
Pratik Kumar <khoonipratik@gmail.com>
2022-11-07 16:45:53 +0530

    done again

--- Frontend/my-app/src/Todo.jsx
+++ Frontend/my-app/src/Todo.jsx
@@ -10,11 +10,17 @@ const Todo = () => {
     });
     const [todos,setTodos] =useState([]);
     useEffect(()=>{
-        fetch(`http://localhost:8005/Todos`).then((res)=>res.json()).then(res=>{
+        fetch(`http://localhost:8005/Todos`,{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json",
+                'authorization':`Bearer ${localStorage.getItem('token')}`
+            }
+        }).then((res)=>res.json()).then(res=>{
             setTodos(res);
             console.log(res)
-        },[])
-    })
+        })
+    },[])
     function Change(e){
        const {name,value} = e.target;
        setTask({
@@ -28,8 +34,8 @@ const Todo = () => {
             method:"POST",
             headers:{
                 "Content-Type":"application/json",
-                "Accept":"application/json",
-                'Authorization':`Bearer ${localStorage.getItem('token')}`
+                
+                'authorization':`Bearer ${localStorage.getItem('token')}`
             },
             body:JSON.stringify(task),
         }).then((res)=>res.json()).then((res)=>{
@@ -40,7 +46,15 @@ const Todo = () => {
 
   return (
     <div>
-         
+         {todos.map((elem)=>{
+            return(
+            <div>
+                <h2>Status :{elem.status}</h2>
+                <h2>tag :{elem.tag}</h2>
+                <h2>taskname :{elem.taskname}</h2>
+            </div>
+            )
+         })}
         Create Todo<br></br>
         <label>Taskname</label>
         <input type="text" name="taskname" onChange={Change}/><br></br>
