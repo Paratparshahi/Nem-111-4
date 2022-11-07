6d4e3588f84779a47da4cee89aca79a6112ba592
Pratik Kumar <khoonipratik@gmail.com>
2022-11-07 15:32:12 +0530

    done

---
+++ Todos/Todo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema =mongoose.Schema;
+
+const todo = new Schema({
+    taskname:{type:String,required:true},
+    status :{type:String,enum:["pending","done"],required:true},
+    tag :{type:String ,enum:["personal", "official", "family"], required:true} })
+
+const Todos = mongoose.model("Todos",todo);
+
+module.exports ={Todos}
