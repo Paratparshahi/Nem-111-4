759a0682be37b6f25412bbde4a676b7acbae7b75
Pratik Kumar <khoonipratik@gmail.com>
2022-11-07 16:36:17 +0530

    done

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -4,11 +4,12 @@ import {  Routes, Route } from "react-router-dom";
 import Signup from './Signup';
 import Login from './Login';
 import Todo from './Todo';
+import { Home } from './Home';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Signup/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/Todo' element={<Todo/>}/>
